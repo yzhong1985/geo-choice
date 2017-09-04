b1ccb3310cb5ed2f46b58d2b8b3f383cb72eb735
yzhong1985 <yzhong1985@gmail.com>
2017-09-04 00:11:02 -0700

    updates for map setup

--- scripts/map.js
+++ scripts/map.js
@@ -1,15 +1,24 @@
 require([
-    "esri/Map",
-    "esri/views/MapView",
+    "esri/map",
+    "esri/layers/FeatureLayer",
+    "esri/renderers/SimpleRenderer",
+    "esri/symbols/PictureMarkerSymbol",
     "dojo/domReady!"
-], function(Map, MapView) {
+], function(Map, FeatureLayer, SimpleRenderer, PictureMarkerSymbol) {
     // Code to create the map and view will go here
-    var map = new Map({
-        basemap: "streets"
+    var map = new Map("viewDiv", {
+        basemap: "topo",
+        zoom: 13,
+        center: [-122.3321, 47.6062],
+        minZoom: 13,
+        maxZoom: 17
     });
-    var view = new MapView({
-        container: "viewDiv",  // Reference to the DOM node that will contain the view
-        map: map               // References the map object created in step 3
-    });
+
+    var symbol = new PictureMarkerSymbol('img/starbucks-icon.png', 20, 20);
+    var featureLayer = new FeatureLayer("https://services.arcgis.com/q3Zg9ERurv23iysr/arcgis/rest/services/Starbucks_Location/FeatureServer/0");
+
+    var renderer = new SimpleRenderer(symbol);
+    featureLayer.setRenderer(renderer);
+    map.addLayer(featureLayer);
 });
 
